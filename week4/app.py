f3d28d2cadbc560f12ee8095cf4b2679ed8e06aa
ChennXIao <61040179+ChennXIao@users.noreply.github.com>
2023-07-27 21:34:40 +0800

    Update app.py

--- week4/app.py
+++ week4/app.py
@@ -17,19 +17,17 @@ def index():
     return render_template("index.html")
 
 @app.route('/signin',methods=["POST"])
-def signin():
+def ver():
     name = request.form.get("account", "")
     password = request.form.get("password", "")
-    agree = request.form.get("checked", "")
-    if agree != "on":
-        print(name,password,agree)
-        return redirect("/")
-    elif(name!="test" or password!= "test"):
-        return redirect("/error?message=請輸入正確的帳號和密碼")
+
+    if name!="test" or password!= "test":
+        return redirect("/error?message=請輸入正確的帳號密碼")
     else:
         session["role"] = name
         return redirect(url_for('member'))
     
+
 @app.route("/member")
 def member():
     if "role" in session:
@@ -38,10 +36,11 @@ def member():
     else:
         return redirect(url_for('index'))
 
+
+#表單和回傳
 @app.route("/error")
 def error():
     message = request.args.get("message", "")
-    print(message)
     return render_template("error.html", message=message)
 
 @app.route("/signout")
@@ -49,24 +48,10 @@ def out():
     session.clear()
     return redirect(url_for('index'))
 
-@app.route('/cal',methods=["POST"])
-def cal():
-    number = request.form.get('number')
-    try:
-        number = int(number)
-        print(type(number))
-        if isinstance(number, int):
-            return redirect(url_for('square', number=number))
-        else:
-            return redirect(url_for('index'))
-    except ValueError:
-        return redirect(url_for('index'))
-
-
-@app.route("/square/<number>")
+@app.route('/square/<number>')
 def square(number):
-        square = int(number)*int(number)
-        return render_template("square.html",square=square)
+    square = int(number)*int(number);
+    return render_template("square.html",square=square)
 
 app.run()
 
